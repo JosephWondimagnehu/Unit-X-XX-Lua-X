6cda4b3810ec686aaa94fa1b711475d98607582d
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-12-15 01:07:44 -0500

    Create Template.lua

---
+++ Template.lua
@@ -0,0 +1,13 @@
+--[[
+-- Explain what your program does.
+--
+-- Name: Name
+-- Version: 1.0
+-- Date Created: Year-Month-Day
+--]]
+
+-- Creates the module.
+local Template = {}
+
+-- Exports the module.
+return Template
