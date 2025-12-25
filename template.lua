a0aeefd6301e8ffd708500b2ce04224909cd088a
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-12-25 13:36:12 -0500

    Update and rename Template.lua to template.lua

---
+++ template.lua
@@ -0,0 +1,13 @@
+--[[
+  Explain what your program does.
+
+  Name: Joseph Wondimagnehu
+  Version: 1.0
+  Date Created: Year-Month-Day
+]]
+
+-- Creates the module.
+local template = {}
+
+-- Exports the module.
+return template
